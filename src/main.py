e0723d5e79c34e3272b9e621eaf04baa6aac2e91
Alexey Fedechkin <alex@centralhardware.ru>
2020-12-22 12:57:33 +0700

    refactor
    (cherry picked from commit 9162940ceeea15b242b0849427e6ab7ed13cd61c)

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from telethon.sync import TelegramClient
 
 
-def str2bool(v):
-    return v.lower() in ("yes", "true", "t", "1")
+def str2bool(boolean_string):
+    return boolean_string.lower() in ("yes", "true", "t", "1")
 
 
 api_id = int(os.getenv('API_ID'))
@@ -27,40 +27,53 @@ class Server(BaseHTTPRequestHandler):
 
     def do_POST(self):
         post_data = self.rfile.read(int(self.headers['Content-Length']))
-        if debug: logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                               str(self.path), str(self.headers), post_data.decode('utf-8'))
+        if debug:
+            logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                         str(self.path), str(self.headers), post_data.decode('utf-8'))
 
         data = json.loads(post_data.decode('utf-8'))
         ping_id = client.send_message(data['username'], '/ping').id
         time.sleep(0.5)
 
         last_id = list(client.iter_messages(data['username'], limit=1))[0].id
-        if debug: logging.info(
-            str("ping_id = " + str(ping_id) + " last_id = " + str(last_id) + " difference = " + str(last_id - ping_id)))
+
+        if debug:
+            logging.info("ping_id = %s last_id = %s difference = %s", str(ping_id), str(last_id),
+                         str(last_id - ping_id))
+
         if last_id > ping_id:
             self._do_response(200)
         else:
             self._do_response(400)
 
 
-def run(server_class=HTTPServer, handler_class=Server, port=8088):
-    logging.basicConfig(level=logging.INFO)
+def run():
     logging.info('start application')
+
     client.connect()
     client.start(phone=telephone)
-    if debug: logging.info("connect to telegram")
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
 
-    if debug: logging.info('Starting httpd...\n')
+    if debug:
+        logging.info("connect to telegram")
+
+    server_address = ('', 8088)
+    httpd = HTTPServer(server_address, Server)
+
+    if debug:
+        logging.info('Starting httpd...\n')
+
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    if debug: logging.info('Stopping httpd...\n')
+
+    if debug:
+        logging.info('Stopping httpd...\n')
+
     client.disconnect()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     run()
